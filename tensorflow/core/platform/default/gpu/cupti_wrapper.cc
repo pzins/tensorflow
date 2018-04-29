efe3c7686caf8b2efaafe008db3728657868e16b
Pierre Zins <zins.pierre@gmail.com>
2018-04-28 23:25:07 -0400

    clean cupti wrapper

--- tensorflow/core/platform/default/gpu/cupti_wrapper.cc
+++ tensorflow/core/platform/default/gpu/cupti_wrapper.cc
@@ -63,37 +63,7 @@ LIBCUPTI_WRAP(cuptiEnableDomain);
 LIBCUPTI_WRAP(cuptiSubscribe);
 LIBCUPTI_WRAP(cuptiUnsubscribe);
 
-// new
-LIBCUPTI_WRAP(cuptiMetricGetIdFromName);
-LIBCUPTI_WRAP(cuptiMetricGetNumProperties);
-LIBCUPTI_WRAP(cuptiMetricGetNumEvents);
-LIBCUPTI_WRAP(cuptiMetricCreateEventGroupSets);
-LIBCUPTI_WRAP(cuptiMetricGetValue);
-LIBCUPTI_WRAP(cuptiMetricGetAttribute);
-LIBCUPTI_WRAP(cuptiSetEventCollectionMode);
-LIBCUPTI_WRAP(cuptiEventGroupSetAttribute);
-LIBCUPTI_WRAP(cuptiEventGroupEnable);
-LIBCUPTI_WRAP(cuptiDeviceGetEventDomainAttribute);
-LIBCUPTI_WRAP(cuptiEventGroupGetAttribute);
-LIBCUPTI_WRAP(cuptiEventGroupReadEvent);
-LIBCUPTI_WRAP(cuptiEventGetAttribute);
-LIBCUPTI_WRAP(cuptiEventGroupDisable);
-LIBCUPTI_WRAP(cuptiActivityEnableLatencyTimestamps);
-
-LIBCUPTI_WRAP(cuptiEventGroupAddEvent);
-LIBCUPTI_WRAP(cuptiEventGetIdFromName);
-LIBCUPTI_WRAP(cuptiEventGroupCreate);
-LIBCUPTI_WRAP(cuptiEventGroupReadAllEvents);
-
 } //namespace dynload
-
-CUptiResult CuptiWrapper::ActivityEnableLatencyTimestamps(uint8_t enable) {
-    return dynload::cuptiActivityEnableLatencyTimestamps(enable);
-}
-CUptiResult CuptiWrapper::EventGroupReadAllEvents(CUpti_EventGroup eventGroup, CUpti_ReadEventFlags flags, size_t* eventValueBufferSizeBytes, uint64_t* eventValueBuffer, size_t* eventIdArraySizeBytes, CUpti_EventID* eventIdArray, size_t* numEventIdsRead) {
-    return dynload::cuptiEventGroupReadAllEvents(eventGroup, flags, eventValueBufferSizeBytes, eventValueBuffer, eventIdArraySizeBytes, eventIdArray, numEventIdsRead);
-}
-
 
 CUptiResult CuptiWrapper::ActivityDisable(CUpti_ActivityKind kind) {
   return dynload::cuptiActivityDisable(kind);
@@ -154,77 +124,6 @@ CUptiResult CuptiWrapper::Subscribe(CUpti_SubscriberHandle* subscriber,
 CUptiResult CuptiWrapper::Unsubscribe(CUpti_SubscriberHandle subscriber) {
   return dynload::cuptiUnsubscribe(subscriber);
 }
-
-
-CUptiResult CuptiWrapper::MetricGetIdFromName(CUdevice device, const char* metricName, CUpti_MetricID* metric) {
-    return dynload::cuptiMetricGetIdFromName(device, metricName, metric);
-}
-
-CUptiResult CuptiWrapper::MetricGetNumProperties(CUpti_MetricID metric, uint32_t* numProp) {
-    return dynload::cuptiMetricGetNumProperties(metric, numProp);
-}
-
-CUptiResult CuptiWrapper::MetricGetNumEvents(CUpti_MetricID metric, uint32_t* numEvents) {
-    return dynload::cuptiMetricGetNumEvents(metric, numEvents);
-}
-
-CUptiResult CuptiWrapper::MetricCreateEventGroupSets(CUcontext context, size_t metricIdArraySizeBytes, CUpti_MetricID* metricIdArray, CUpti_EventGroupSets** eventGroupPasses) {
-    return dynload::cuptiMetricCreateEventGroupSets(context, metricIdArraySizeBytes, metricIdArray, eventGroupPasses);
-}
-
-CUptiResult CuptiWrapper::MetricGetValue(CUdevice device, CUpti_MetricID metric, size_t eventIdArraySizeBytes, CUpti_EventID* eventIdArray, size_t eventValueArraySizeBytes, uint64_t* eventValueArray, uint64_t timeDuration, CUpti_MetricValue* metricValue) {
-    return dynload::cuptiMetricGetValue(device, metric, eventIdArraySizeBytes, eventIdArray, eventValueArraySizeBytes, eventValueArray, timeDuration, metricValue);
-}
-
-CUptiResult CuptiWrapper::MetricGetAttribute(CUpti_MetricID metric, CUpti_MetricAttribute attrib, size_t* valueSize, void* value) {
-    return dynload::cuptiMetricGetAttribute(metric, attrib, valueSize, value);
-}
-
-CUptiResult CuptiWrapper::SetEventCollectionMode(CUcontext context, CUpti_EventCollectionMode mode) {
-    return dynload::cuptiSetEventCollectionMode(context, mode);
-}
-
-CUptiResult CuptiWrapper::EventGroupSetAttribute(CUpti_EventGroup eventGroup, CUpti_EventGroupAttribute attrib, size_t valueSize, void* value) {
-    return dynload::cuptiEventGroupSetAttribute(eventGroup, attrib, valueSize, value);
-}
-
-CUptiResult CuptiWrapper::EventGroupEnable(CUpti_EventGroup eventGroup) {
-    return dynload::cuptiEventGroupEnable(eventGroup);
-}
-
-CUptiResult CuptiWrapper::DeviceGetEventDomainAttribute(CUdevice device, CUpti_EventDomainID eventDomain, CUpti_EventDomainAttribute attrib, size_t* valueSize, void* value) {
-    return dynload::cuptiDeviceGetEventDomainAttribute(device, eventDomain, attrib, valueSize, value);
-}
-
-CUptiResult CuptiWrapper::EventGroupGetAttribute(CUpti_EventGroup eventGroup, CUpti_EventGroupAttribute attrib, size_t* valueSize, void* value) {
-    return dynload::cuptiEventGroupGetAttribute(eventGroup, attrib, valueSize, value);
-}
-
-CUptiResult CuptiWrapper::EventGroupReadEvent(CUpti_EventGroup eventGroup, CUpti_ReadEventFlags flags, CUpti_EventID event, size_t* eventValueBufferSizeBytes, uint64_t* eventValueBuffer) {
-    return dynload::cuptiEventGroupReadEvent(eventGroup, flags, event, eventValueBufferSizeBytes, eventValueBuffer);
-}
-
-
-CUptiResult CuptiWrapper::EventGroupDisable(CUpti_EventGroup eventGroup) {
-    return dynload::cuptiEventGroupDisable(eventGroup);
-}
-
-CUptiResult CuptiWrapper::EventGetAttribute(CUpti_EventID event, CUpti_EventAttribute attrib, size_t* valueSize, void* value) {
-    return dynload::cuptiEventGetAttribute(event, attrib, valueSize, value);
-}
-
-CUptiResult CuptiWrapper::EventGroupAddEvent(CUpti_EventGroup eventGroup, CUpti_EventID event) {
-    return dynload::cuptiEventGroupAddEvent(eventGroup, event);
-}
-
-CUptiResult CuptiWrapper::EventGetIdFromName(CUdevice device, const char* eventName, CUpti_EventID* event) {
-    return dynload::cuptiEventGetIdFromName(device, eventName, event);
-}
-
-CUptiResult CuptiWrapper::EventGroupCreate(CUcontext context, CUpti_EventGroup* eventGroup, uint32_t flags) {
-    return dynload::cuptiEventGroupCreate(context, eventGroup, flags);
-}
-
 
 }  // namespace profiler
 }  // namespace gputools
